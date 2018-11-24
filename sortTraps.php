67b2b2abe3e87c8ac92ed40c04ba22db59a98818
hulisani-mudimeli <hulisanimudimeli@gmail.com>
2018-11-24 09:07:04 +0200

    Return value changed

--- sortTraps.php
+++ sortTraps.php
@@ -14,9 +14,9 @@ $sortBy = $_POST['sortBy'];
 //TODO
 
 if($sortBy == 0)
-    $sql = "SELECT  client_id, hardware_id, token, type, Upload_Interval, caught from device ORDER BY caught DESC;";
+    $sql = "SELECT  * from device ORDER BY caught DESC;";
 else if($sortBy == 1)
-    $sql = "SELECT  client_id, hardware_id, token, type, Upload_Interval, caught , tempCount from device ORDER BY tempCount DESC;";
+    $sql = "SELECT  * from device ORDER BY tempCount DESC;";
 
 $result = $conn->query($sql);
 $counter = 0;
@@ -28,8 +28,9 @@ if($result->num_rows > 0)
         # code...
         if($row["client_id"] == $active_client)
         {
-            $result_array[] = array($row["hardware_id"],$row["caught"],$row["type"],$row["Upload_Interval"]);
-            //$returnedString .=  $row["hardware_id"].' '.$row["type"]."\n";
+//            $result_array[] = array($row["hardware_id"],$row["caught"],$row["type"],$row["Upload_Interval"]);
+            $result_array[] = array($row["hardware_id"],$row["caught"],$row["type"],$row["Upload_Interval"],$row["Battery_Percent"], $row["device_id"]);
+//$returnedString .=  $row["hardware_id"].' '.$row["type"]."\n";
         }
         $counter = $counter + 1;
     }
