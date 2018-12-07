59d2d531db8ae916dc913b4313be6e233165724c
hulisani-mudimeli <hulisanimudimeli@gmail.com>
2018-12-07 10:42:55 +0200

    Merge branch 'master' of C:\wamp64\www\Erbium-Web-App with conflicts.

--- signup.php
+++ signup.php
@@ -15,26 +15,42 @@ if ($conn->connect_error) {
 
 $active_fullname = $_POST['fullname'];
 $active_username = $_POST['username'];
-$active_password = $_POST['password'];
-$active_latitude = $_POST['clientLat'];
-$active_longitude = $_POST['clientLong'];
+$active_latitude = $_POST['latitude'];
+$active_longitude = $_POST['longitude'];
 
 /*if(strlen($active_password)<8){
     echo 'The password should be at least 8 characters long';
     exit();
 }*/
 
+$active_password = substr(md5(uniqid(mt_rand(), true)), 0, 8);
+
 $escapedPW = mysqli_real_escape_string($conn, $active_password);
 $salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
 $saltedPW = $escapedPW.$salt;
 $hashedPW = hash('sha256', $saltedPW);
 
+$mailMessage = 'Hi '.$active_fullname.', your auto generated Erbium password is '.$active_password.'.';
+$mailMessage = wordwrap($mailMessage, 70);
+if(mail($active_username, "Erbium Registration", $mailMessage))
+    echo "Sent";
+else
+    echo "Not sent";
+return;
+
+$tempStr = $active_fullname.'-'.$active_password;
+
+//mail("hulisanimudimeli@gmail.com", "Erbium Something", "Hi bro");
 $sql = "INSERT INTO client(full_name, username, password, salt, latitude, longitude, Admin)
-		VALUES ('$active_fullname', '$active_username', '$hashedPW', '$salt','$active_latitude','$active_longitude', 0)";
+		VALUES ('$tempStr', '$active_username', '$hashedPW', '$salt','$active_latitude','$active_longitude', 0)";
 
 //echo $active_fullname.'-'.$active_username.'-'.$hashedPW.'-'.$salt;
 
 if($conn->query($sql) === true){
+    $mailMessage = 'Hi '.$active_fullname.', your auto generated Erbium password is '.$active_password.'.';
+    $mailMessage = wordwrap($mailMessage, 70);
+    mail($active_username, "Erbium Registration", $mailMessage);
+
     $conn->close();
     echo "New user successfully added!";
 }
@@ -42,3 +58,4 @@ else{
     echo $conn->error;
     $conn->close();
 }
+return;
